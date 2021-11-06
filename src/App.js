cf52f673f6589f66bb63c203c401cd909e9bd5e5
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-11-06 11:44:21 +0530

    home, notFoundPage component completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -21,20 +21,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
-
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
-
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
